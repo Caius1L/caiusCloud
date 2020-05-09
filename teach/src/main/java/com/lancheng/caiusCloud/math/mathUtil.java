6534eb699b9d069e216f1a49e88bd7ecfce0ce6e
蓝城 <lancheng@ruhrtec.cn>
2020-05-09 15:17:19 +0800

    添加了一个专门用来测试的模块

---
+++ teach/src/main/java/com/lancheng/caiusCloud/math/mathUtil.java
@@ -0,0 +1,27 @@
+package com.lancheng.caiusCloud.math;
+
+public class mathUtil {
+//
+//    def taylorSin(n, d):
+//            # calculates sin(n) using a taylor-
+//            # polynomial of degree d
+//
+//    # implement some sort of modulus to shift n in-
+//            # to the domain of [0, 2*pi] or [-pi, pi]
+//
+//            return helper(n, d, 0)
+//
+//    def helper(n, d, x):
+//            # helper function for taylorSin(n, d)
+//    if d < 1:
+//            return x
+//    else:
+//            if d % 4 == 1:
+//            return helper(n, d - 2, x + float(n**d)/f(d))
+//    elif d % 4 == 3:
+//            return helper(n, d - 2, x + -1*float(n**d)/f(d))
+//    print 'Something screwed up'
+//            return None
+//
+//            testval = 2.13
+}
