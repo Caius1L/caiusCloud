a18287588c06baee69e428dd647cfd8efbf5650d
蓝城 <lancaius@outlook.com>
2020-10-28 22:43:39 +0800

    添加了简单计算器的实现

--- teach/src/main/java/com/lancheng/caiusCloud/code/calculate.java
+++ teach/src/main/java/com/lancheng/caiusCloud/code/calculate.java
@@ -1,182 +1,223 @@
 package com.lancheng.caiusCloud.code;
 
-import org.springframework.util.StringUtils;
-
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
-import java.util.StringTokenizer;
+import java.util.Scanner;
 
-/**
- * 计算机的实现
- */
 public class calculate {
-
     public static void main(String[] args) {
-        System.out.println(exec("-8*(((-2+4)+3)/((-1-5)*-2)-5)"));
 
+        Scanner input = new Scanner(System.in);
+        String str = input.nextLine();
+        String[] inOrderArrays = strToArrays(str);
+        System.out.println(Arrays.toString(inOrderArrays));
+        String[] postOrderArrays = toPostOrder(inOrderArrays);
+        System.out.println(Arrays.toString(inOrderArrays));
+        Double result = toCompute(postOrderArrays);
+        System.out.printf("%.3f",result);
     }
 
-    /**
-     * @param exp 带括号的四则表达式
-     * @return 运算结果
-     */
-    public static double exec(String exp) {
+    /*
+    将字符串分割成操作数和操作符的字符串数组
+    *//*
+    将字符串分割成操作数和操作符的字符串数组
+    */
+    public static String[] strToArrays(String str) {
+        int strLength = str.length();
+        int beginIndex = 0;
+        int endIndex = 0;
+        String[] Arrays = new String[strLength];
+        int arraysIndex = 0;
 
-        int leftIndex = exp.lastIndexOf('('); // 16
-
-        if (leftIndex == -1) {
-            // 没有括号
-            // System.out.println("calc" + exp);
-            return calc(exp);
-        } else {
-            // 如果有括弧，调用exec
-            // System.out.println("exec" + exp);
-            // 先找最里面的(位置 再找对应的)位置
-
-            // (-1-5)*-2)-5) 21
-            int rightIndex = exp.substring(leftIndex).indexOf(')') + leftIndex;
-
-            // 去独立的表达式，运算 calc（-1-5）
-            double res = calc(exp.substring(leftIndex + 1, rightIndex));
-            // 重新组织表达式
-            exp = exp.substring(0, leftIndex) + res
-                    + exp.substring(rightIndex + 1);
-            // -8*(((-2+4)+3)/( -6 *-2)-5)
-            return exec(exp);
-        }
-
-    }
-
-    /**
-     * @param exp 不带括号的四则表达式
-     * @return 运算结果
-     */
-    public static double calc(String exp) {
-        // 1 . 获取所有四则运算的数字
-        List<Double> numbers = sliptNumbers(exp);
-        // 2. 获取所有四则运算的操作符号
-        List<Character> ops = sliptOps(exp);
-        // 3. 先乘车运算
-        // 遍历运算符中的*和/
-        for (int i = 0; i < ops.size(); i++) {
-            // * /
-            // 获取运算符（不移除）
-            char op = ops.get(i);
-
-            // 如果是 * 或者 /， 从运算符的容器中移除，同是从数字容器中到对应该运算符位置的两个数字（移除数据，后面所有数据往前顺序移动）
-            if (op == '*' || op == '/') {
-                // 从运算符的容器中移除
-                ops.remove(i);// 移除当前位置
-
-                // 从数字容器中获取对应该运算符位置的两个数字（移除）
-                double d1 = numbers.remove(i);
-                double d2 = numbers.remove(i);
-
-                // 运算
-                d1 = op == '*' ? d1 * d2 : d1 / d2;
-
-                // 把运算结果插入到数字容器中的i位置
-                numbers.add(i, d1);// 插入到i的位置 原来从i位置一直到最后的数据，都要往后瞬移一位
-                // numbers.set(i, d1);//设置i位置上的数据为d1,其余不变
-                i--;// 移除后，后面所有运算符往前移动，为了保证下一个运算符不被遗漏，所以i--
-            }// end if (op == '*' || op == '/') {
-
-        }// end for (int i = 0 ; i < ops.size(); i++) {
-
-        // 4. 再加减运算
-        while (!ops.isEmpty()) {
-            // 每次去运算容器中第一个运算符
-            char op = ops.remove(0);
-            // 每次从数字容器中两次取第一个数字
-            double d1 = numbers.remove(0);
-            double d2 = numbers.remove(0);
-
-            // 计算
-            d1 = op == '+' ? d1 + d2 : d1 - d2;
-
-            // 把结果插入到数字容器中的第一个位置
-            numbers.add(0, d1);
-        }
-
-        // 5. 返回结果
-
-        return numbers.get(0);
-    }
-
-    /**
-     * 从表达式中分离所有的运算符
-     *
-     * @param exp
-     */
-    private static List<Character> sliptOps(String exp) {
-        List<Character> ops = new ArrayList<Character>();
-        // -8*-2+3/-1-5*-2-5
-        // 把真实表达式变成下面的表达式
-        String formaterExp = formaterExp(exp);
-        // @8*@2+3/@1-5*@2-5
-
-        StringTokenizer st = new StringTokenizer(formaterExp, "@0123456789.");
-        while (st.hasMoreTokens()) {
-            String opStr = st.nextElement() + "";// 取出分割符号之间的数据
-            // System.out.println(numStr);
-            // 如果前面是@ 变为负数
-            ops.add(opStr.charAt(0));
-        }
-        return ops;
-    }
-
-    /**
-     * 从表达式中分离所有的数字
-     *
-     * @param exp -8*-2+3/-1-5*-2-5 表达式
-     */
-    private static List<Double> sliptNumbers(String exp) {
-        List<Double> numbers = new ArrayList<Double>();
-        // -8*-2+3/-1-5*-2-5
-        // 把真实表达式变成下面的表达式
-        String formaterExp = formaterExp(exp);
-        // @8*@2+3/@1-5*@2-5
-
-        StringTokenizer st = new StringTokenizer(formaterExp, "+-*/");
-        while (st.hasMoreTokens()) {
-            String numStr = st.nextElement() + "";// 取出分割符号之间的数据
-            // System.out.println(numStr);
-            // 如果前面是@ 变为负数
-            if (numStr.charAt(0) == '@') {
-                numStr = "-" + numStr.substring(1);
-            }
-
-            // 把数字型的字符串变成数字
-            numbers.add(Double.parseDouble(numStr));
-        }
-        return numbers;
-    }
-
-    private static String formaterExp(String exp) {
-        int index = 0;
-        while (index < exp.length()) {
-            char c = exp.charAt(index);
-            // 判断是否是-符号
-            // -代表的是负数 第一个，前一字符*/
-            if (c == '-') {
-                // 第一个，
-                if (index == 0) {
-                    exp = "@" + exp.substring(1);
+        for (int i = 0; i < strLength; i++) {
+            if (str.charAt(i) == '*' || str.charAt(i) == '/' || str.charAt(i) == '+' || str.charAt(i) == '-' || str.charAt(i) == '(' || str.charAt(i) == ')') {
+                endIndex = i - 1;
+                if (beginIndex <= endIndex) {
+                    Arrays[arraysIndex] = str.substring(beginIndex, endIndex + 1);
+                    Arrays[arraysIndex + 1] = String.valueOf(str.charAt(i));
+                    arraysIndex += 2;
+                    beginIndex = i + 1;
                 } else {
-                    // 前一字符* /
-                    if (exp.charAt(index - 1) == '*'
-                            || exp.charAt(index - 1) == '/') {
-                        exp = exp.substring(0, index) + "@"
-                                + exp.substring(index + 1);
+                    Arrays[arraysIndex] = String.valueOf(str.charAt(i));
+                    arraysIndex += 1;
+                    beginIndex = i + 1;
+                }
+            }
+        }//简单的判断一下表达式是否是以操作符结尾还是操作数结尾
+        if (!isOper(String.valueOf(str.charAt(strLength - 1)))) {
+            Arrays[arraysIndex] = str.substring(beginIndex, strLength);
+            arraysIndex += 1;
+        }
+        String[] Arrays2 = new String[arraysIndex];
+        for (int i = 0; i < arraysIndex; i++) {
+            Arrays2[i] = Arrays[i];
+        }
+        System.out.println(arraysIndex);
+        return Arrays2;
+    }
+
+    /*
+    将中缀表达式转为后缀表达式,返回的是字符串数组
+    */
+    public static String[] toPostOrder(String[] arrays) {
+        /*规则：
+         *1，运算数直接输出
+         *2，左括号压入堆栈
+         *3，右括号 将栈顶的运算符弹出并输出，括号出栈不输出
+         *4，运算符：
+         *    若优先级大于栈顶运算符，压入栈
+         *    若优先级小于或等于栈顶运算符，栈顶运算符弹出并输出，
+         *       继续和新栈顶比较，直到比栈顶运算符优先级大，将它压入栈
+         *5，对象处理完毕后，将栈中运算符弹出并输出
+         */
+        Stack operStack = new Stack();//创建了一个操作符的栈
+        int arraysLength = arrays.length;
+        String[] arrays2 = new String[arraysLength];//输出后的字符数组
+        int tempIndex = 0;
+
+        //将字符串数组遍历
+        for (int i = 0; i < arraysLength; i++) {
+            //操作符入栈
+            if (isOper(arrays[i])) {
+                //栈为空时直接入栈
+                if (operStack.isEmpty()) {
+                    operStack.push(arrays[i]);
+                } else {
+                    //操作符为"("时直接入栈
+                    if (arrays[i].equals("(")) {
+                        operStack.push(arrays[i]);
+                    }
+                    //操作符为")"时栈顶出栈并输出，直到遇到"(", "("出栈,")"不入栈
+                    else if (arrays[i].equals(")")) {
+                        while (operStack.peek().equals("(") == false) {
+                            arrays2[tempIndex] = operStack.pop();
+                            tempIndex += 1;
+                        }
+                        operStack.pop();//"("出栈
+                    }
+                    //其他操作符需要比较与栈顶的优先级
+                    else {
+                        //栈顶是"(", 直接入栈
+                        if (operStack.peek().equals("(")) {
+                            operStack.push(arrays[i]);
+                        } else {
+                            //优先级高，直接入栈
+                            if (getPriority(arrays[i].charAt(0)) > getPriority(operStack.peek().charAt(0))
+                                    && operStack.isEmpty() == false) {
+                                operStack.push(arrays[i]);
+                            }
+                            //优先级低或者相等，栈顶出栈并输出，直到优先级比栈顶高
+                            else {
+                                while (getPriority(arrays[i].charAt(0)) <= getPriority(operStack.peek().charAt(0))
+                                        && operStack.isEmpty() == false) {
+                                    arrays2[tempIndex] = operStack.pop();
+                                    tempIndex += 1;
+                                    //若栈顶全部弹出，则直接入栈
+                                    if (operStack.isEmpty()) {
+                                        operStack.push(arrays[i]);
+                                        break;
+                                    }
+                                }
+                                if (getPriority(arrays[i].charAt(0)) > getPriority(operStack.peek().charAt(0))) {
+                                    operStack.push(arrays[i]);
+                                }
+                            }
+                        }
                     }
                 }
             }
-
-            index++;
-            //
+            //操作数直接添加到 字符串数组2
+            else if (isNum(arrays[i])) {
+                arrays2[tempIndex] = arrays[i];
+                tempIndex += 1;
+            } else {
+            }
         }
-        return exp;
+        while (!operStack.isEmpty()) {
+            arrays2[tempIndex] = operStack.pop();
+            tempIndex += 1;
+        }
+        String[] arrays3 = new String[tempIndex];
+        for (int i = 0; i < tempIndex; i++) {
+            arrays3[i] = arrays2[i];
+        }
+        return arrays3;
     }
 
+    /*
+    得到操作符的优先级
+    */
+    public static int getPriority(char c) {
+        if (c == '*' || c == '/') {
+            return 2;
+        } else if (c == '+' || c == '-') {
+            return 1;
+        } else {
+            return 999;
+        }
+    }
+
+    /*
+    由后缀表达式计算得值
+    */
+    public static double toCompute(String[] arrays) {
+        /*规则：
+         *中缀表达式不用比较优先级
+         *将运算数入栈，每读到一个运算符
+         *就弹出栈顶的两个运算数，运算完毕后将结果压入栈
+         */
+        Stack numStack = new Stack();//创建了一个操作数的栈
+        int arraysLength = arrays.length;
+        //遍历后缀表达式的字符串数组
+        for (int i = 0; i < arraysLength; i++) {
+            if (isNum(arrays[i])) {
+                numStack.push(arrays[i]);
+            } else if (isOper(arrays[i])) {
+                Double num2 = Double.parseDouble(numStack.pop());
+                Double num1 = Double.parseDouble(numStack.pop());
+                if (arrays[i].equals("+")) {
+                    Double result = num1 + num2;
+                    numStack.push(result.toString());
+                } else if (arrays[i].equals("-")) {
+                    Double result = num1 - num2;
+                    numStack.push(result.toString());
+                } else if (arrays[i].equals("*")) {
+                    Double result = num1 * num2;
+                    numStack.push(result.toString());
+                } else if (arrays[i].equals("/")) {
+                    Double result = num1 / num2;
+                    numStack.push(result.toString());
+                } else {
+                }
+            } else {
+            }
+        }
+        Double result = Double.parseDouble(numStack.pop());
+        return result;
+    }
+
+    /*
+    判断该字符串是否为操作符
+    */
+    public static boolean isOper(String str) {
+        if (str.equals("*") || str.equals("/") ||
+                str.equals("+") || str.equals("-") ||
+                str.equals("(") || str.equals(")")) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    /*
+    判断该字符串是否为操作数
+    */
+    public static boolean isNum(String str) {
+        if (str.equals("*") || str.equals("/") ||
+                str.equals("+") || str.equals("-") ||
+                str.equals("(") || str.equals(")")) {
+            return false;
+        } else {
+            return true;
+        }
+    }
 }
