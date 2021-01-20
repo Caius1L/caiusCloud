9f7d028538d211ad6c2a3f7ffcbc8fbf633dab2a
蓝城 <lancaius@outlook.com>
2021-01-20 13:52:39 +0800

    新增了设计模式-享元模式的demo

---
+++ teach/src/main/java/com/lancheng/caiusCloud/DesignPattern/Cache/Tree.java
@@ -0,0 +1,23 @@
+package com.lancheng.caiusCloud.DesignPattern.Cache;
+
+import java.awt.*;
+
+/**
+ * 包含每棵树独特的状态
+ */
+public class Tree {
+
+    private int x;
+    private int y;
+    private TreeType type;
+
+    public Tree(int x, int y, TreeType type) {
+        this.x = x;
+        this.y = y;
+        this.type = type;
+    }
+
+    public void draw(Graphics g) {
+        type.draw(g, x, y);
+    }
+}
